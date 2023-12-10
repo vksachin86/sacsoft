93f652ce7ba1e475132e238603fd3bbb578c6ac3
Vksachin <vksachin68@example.com>
2023-12-10 23:13:31 +0530

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactqwertyuiop
+          Learn React
         </a>
       </header>
     </div>
