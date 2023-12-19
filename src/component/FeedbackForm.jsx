aaac410fa93fce53e65a798c4d873e90f4267096
Sachin Vishwakarma <vksachin86@gmail.com>
2023-12-20 00:52:02 +0530

    validation

--- src/component/FeedbackForm.jsx
+++ src/component/FeedbackForm.jsx
@@ -1,11 +1,26 @@
 import React, { useState } from 'react'
-import PropTypes from 'prop-types'
 import Card from './shared/Card'
+import Button from './shared/Button'
 
 function FeedbackForm() {
     const [text,setText] = useState('')
+    const [btnDisabled,setBtnDisabled] = useState('true')
+    const [message,setMessage] = useState('')
 
     const handleTextChange = (a) => {
+        if(text === ''){
+            setBtnDisabled(true)
+            setMessage(null)
+        }
+        else if(text !== '' && text.trim().length<=10 )
+        {
+            setMessage('Text must be atleast 10 character')
+            setBtnDisabled(true)
+
+        }else{
+            setMessage(null)
+            setBtnDisabled(false)
+        }
         setText(a.target.value)
     }
 
@@ -15,12 +30,11 @@ function FeedbackForm() {
         {/* @todo rating select component */}
         <div className="input-group">
             <input onChange={handleTextChange} type="text" placeholder='Write a review' value={text} />
-            <button type='submit'>send</button>
+            <Button type='submit' isDisabled={btnDisabled}>Send</Button>
         </div>
+        {message && <div className='message'>{message}</div>}
     </Card>
   )
 }
-
-FeedbackForm.propTypes = {}
 
 export default FeedbackForm
